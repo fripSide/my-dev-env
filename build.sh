4a6187bb95773b8f6f7259eb9f8463faa755b1b9
fripSide <clangllvm@126.com>
2024-10-25 22:15:41 +0800

    update build try-5

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ubuntu_version=$1
-arch=$2
 
 # docker build --build-arg UBUNTU_VERSION=$ubuntu_version -t ubuntu:$ubuntu_version .
-docker buildx create --use --name multi-arch-builder
-docker buildx build --platform=linux/amd64,linux/arm64 --build-arg UBUNTU_VERSION=$ubuntu_version ARCH=$arch -t ubuntu-$arch:$ubuntu_version .
+
+docker buildx build --platform=linux/amd64 --build-arg UBUNTU_VERSION=$ubuntu_version ARCH=amd64 -t ubuntu-amd64:$ubuntu_version .
+docker buildx build --platform=linux/arm64 --build-arg UBUNTU_VERSION=$ubuntu_version ARCH=arm64v8 -t ubuntu-arm64:$ubuntu_version .
